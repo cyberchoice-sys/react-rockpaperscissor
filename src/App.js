ababd65326c879cd5dc9bab1c809c906e99ba229
cyberchoice-sys <sso-youn@hanmail.net>
2025-03-31 21:19:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Box from "./components/Box"
+
+const choice = {
+  rock:{
+    name : "Rock",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUy3ba0jCVyryr8zNanLW2niF3B1ETwRd4gQ&s",
+
+  },
+  scissor:{
+    name : "Scissor",
+    img: "https://i.pinimg.com/474x/a6/1d/ea/a61dea1843cae2edbf0ec2e71d16d1eb.jpg"
+    },
+  paper:{
+    name : "Paper",
+
+    img: "https://img.freepik.com/free-vector/hand-drawn-flat-paper-cartoon-illustration_23-2151358276.jpg",
+  },
+};
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null)
+
+
+  const play=(userChoice)=>{
+    // console.log("selected!")
+    setUserSelect(choice[userChoice])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <Box title="You" item={userSelect}/>
+        {/* <Box title="Computer" item={}/>  */}
+      </div>
+      <div className="main">
+          <button onClick={() => play("rock")}>Rock</button>
+          <button onClick={() => play("paper")}>Paper</button>
+          <button onClick={() => play("scissor")}>Scissor</button>
+      </div>
     </div>
   );
 }
